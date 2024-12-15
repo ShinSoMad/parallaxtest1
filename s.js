c334ca13df30b14e64284bdd96a1b1ba026f759a
Shinxxdd <122793490+ShinSoMad@users.noreply.github.com>
2024-12-15 22:16:54 +0200

    Update s.js

--- s.js
+++ s.js
@@ -1,1 +1,30 @@
+const pageOne = document.getElementById("pageOne")
+const pageTwo = document.getElementById("pageTwo")
+const pageThre = document.getElementById("pageThre")
+const pageFour = document.getElementById("pageFour")
+
+function foter(div){
+    if(div == pageOne){
+        pageOne.classList.add("active")
+        pageThre.classList.remove("active")
+        pageFour.classList.remove("active")
+        pageTwo.classList.remove("active")
+    }else if(div == pageTwo){
+        pageOne.classList.remove("active")
+        pageThre.classList.remove("active")
+        pageFour.classList.remove("active")
+        pageTwo.classList.add("active")
+    }else if(div == pageThre){
+        pageOne.classList.remove("active")
+        pageThre.classList.add("active")
+        pageFour.classList.remove("active")
+        pageTwo.classList.remove("active")
+    }
+    else if(div == pageFour){
+        pageOne.classList.remove("active")
+        pageThre.classList.remove("active")
+        pageFour.classList.add("active")
+        pageTwo.classList.remove("active")
+    }
+}
 
