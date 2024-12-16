0493d0abb513cad7c962a1e0043f85c2aec9be10
Shinxxdd <122793490+ShinSoMad@users.noreply.github.com>
2024-12-16 21:30:34 +0200

    Add files via upload

--- s.js
+++ s.js
@@ -11,21 +11,21 @@ function foter(div){
         pageFour.classList.remove("active")
         pageTwo.classList.remove("active")
         pageFive.classList.remove("active")
-        //document.location.href = "farm_page.html"
+        document.location.href = "farm_page.html"
     }else if(div == pageTwo){
         pageOne.classList.remove("active")
         pageThre.classList.remove("active")
         pageFour.classList.remove("active")
         pageFive.classList.remove("active")
         pageTwo.classList.add("active")
-        //document.location.href = "tasks.html"
+        document.location.href = "tasks.html"
     }else if(div == pageThre){
         pageOne.classList.remove("active")
         pageThre.classList.add("active")
         pageFour.classList.remove("active")
         pageTwo.classList.remove("active")
         pageFive.classList.remove("active")
-        //document.location.href = "referal_page.html"
+        document.location.href = "referal_page.html"
     }
     else if(div == pageFour){
         pageOne.classList.remove("active")
@@ -33,7 +33,7 @@ function foter(div){
         pageFour.classList.add("active")
         pageTwo.classList.remove("active")
         pageFive.classList.remove("active")
-        //document.location.href = "connect_wallet.html"
+        document.location.href = "connect_wallet.html"
         
     }
     else if(div == pageFive){
